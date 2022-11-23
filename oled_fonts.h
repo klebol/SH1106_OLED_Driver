e50b58df1ac36f21e5cac5f8e24053df97942af5
klebol <miqix99@gmail.com>
2022-11-23 20:16:14 +0100

    Added font files

---
+++ oled_fonts.h
@@ -0,0 +1,10 @@
+#ifndef OLED_FONTS_H_
+#define OLED_FONTS_H_
+#include <avr/pgmspace.h>
+
+#define MAX_FONT_WIDTH	6
+#define MAX_FONT_HEIGHT	8
+extern const uint8_t StandardASCII [] PROGMEM;
+
+
+#endif /* OLED_FONTS_H_ */
